0c0fa4dd036b7f1aac81974e4b527cc2f9da6aa5
Rohit <gittraining@yopmail.com>
2018-08-10 19:11:20 -0400

    Completed one Program

--- src/com/programs/interview/SumOfDigits.java
+++ src/com/programs/interview/SumOfDigits.java
@@ -4,6 +4,15 @@ public class SumOfDigits {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int number=12345;
+		int sum=0;
+		while(number!=0)
+		{
+			int remainder=number%10;
+			sum=sum+remainder;
+			number=number/10;
+		}
+		System.out.println("The Sum of digits are:" +sum);
 
 	}
 

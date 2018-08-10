a124417eda57739fc715bbab5a63b77fd06bbc81
Rohit Bhatt <rohit.bhattt@test.com>
2018-08-10 19:56:02 -0400

    Added Palindrome Program

--- src/com/programs/interview/SumOfDigits.java
+++ src/com/programs/interview/SumOfDigits.java
@@ -4,16 +4,17 @@ public class SumOfDigits {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int number=12345;
-		int sum=0;
-		while(number!=0)
+		int number=1;
+		System.out.println("The Sum of digits are:"+ sumOfDigits(number));
+	}
+		
+	//sum of digit through recursion
+	public static int sumOfDigits(int num)
 		{
-			int remainder=number%10;
-			sum=sum+remainder;
-			number=number/10;
+			if(num==0)
+				return 0;
+				else
+					return (num%10+sumOfDigits(num/10));
 		}
-		System.out.println("The Sum of digits are:" +sum);
-
 	}
 
-}

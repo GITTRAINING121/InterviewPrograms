32bceeb726d4fe3a1585fb54f23042b8d22bf9a2
Rohit Bhatt <rohit.bhattt@test.com>
2018-08-11 00:24:34 -0400

    Added Factorial

--- src/com/programs/interview/Palindrome.java
+++ src/com/programs/interview/Palindrome.java
@@ -1,22 +1,27 @@
 package com.programs.interview;
 
 public class Palindrome {
-		
-	public static void main(String args[])
-	{
-		String name="iTopiNonAvevAnoNipoTi";
-		System.out.println(palindrome(name));
-		
-	}
-	
-	//Recursive method that verifies whether a string passed as a parameter is a palindrome
-	public static boolean palindrome(String s) {
-		  if (s.length() <= 1)
-		    return true;
-		  else
-		    return (s.charAt(0) == s.charAt(s.length()-1)) &&
-		            palindrome(s.substring(1,s.length()-1));// substring(int startIndex, int endIndex) -startIndex: inclusive endIndex: exclusive
-		} 
 
+	public static void main(String args[]) {
+		String name = "iTopiNonAvevAnoNipoTi";
+		System.out.println(palindrome(name));
+
+	}
+
+	// Recursive method that verifies whether a string passed as a parameter is a
+	// palindrome
+	public static boolean palindrome(String s) {
+		if (s.length() <= 1)
+			return true;
+		else
+			return (s.charAt(0) == s.charAt(s.length() - 1)) && palindrome(s.substring(1, s.length() - 1));// substring(int
+																											// startIndex,
+																											// int
+																											// endIndex)
+																											// -startIndex:
+																											// inclusive
+																											// endIndex:
+																											// exclusive
+	}
 
 }
